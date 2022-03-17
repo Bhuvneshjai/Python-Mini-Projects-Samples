57947c9c295d0bbb1a9a575b7de15ff084318cfb
Bhuvnesh Jain <82877515+Bhuvneshjai@users.noreply.github.com>
2022-03-18 00:10:56 +0530

    Add files via upload
    Python Program for sorting by use algorithms module

---
+++ Simple Python Projects/Algorithm_Module.py
@@ -0,0 +1,6 @@
+# Algorithms module contains various algorithms and operations such as searching, sorting,
+# factorial etc.
+from algorithms.sort import merge_sort
+list1 = [11,3,44,6,9,7]
+sorted_list = merge_sort(list1)
+print(sorted_list)
